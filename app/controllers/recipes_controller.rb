69a15ab688bf9075251b7fc678375e0684597edd
Luis <lriverawong@outlook.com>
2017-08-28 15:38:16 -0400

    finished added security for CRUD

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,24 +1,33 @@
 class RecipesController < ApplicationController
+  before_action :require_login
+
+  # made changes to only allow a user to edit their posts
+  # "Recipe.all" became "current_user.recipes"
+  # "Recipe.new" became "current_user.recipes.build"
+  # "Recipe.find" became "current_user.recipes.find"
+
   def index
-    @recipes = Recipe.all
+    @recipes = current_user.recipes.order(:id)
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
+    @recipe = current_user.recipes.find(params[:id])
   end
 
   def destroy
-    recipe = Recipe.find(params[:id])
+    recipe = current_user.recipes.find(params[:id])
     recipe.destroy
     redirect_to recipes_path, notice: "Deleted Recipe: #{recipe.name}"
   end
 
   def new
-    @recipe = Recipe.new
+    #@recipe = Recipe.new
+    @recipe = current_user.recipes.build
   end
 
   def create
-    @recipe = Recipe.new(recipe_params)
+    #@recipe = Recipe.new(recipe_params)
+    @recipe = current_user.recipes.build(recipe_params)
 
     if @recipe.save
       redirect_to recipe_path(@recipe), notice: "Recipe Created!"
@@ -28,15 +37,31 @@ class RecipesController < ApplicationController
     end
   end
 
-  private
-    # Using a private method to encapsulate the permissible parameters is
-    # a good pattern since you'll be able to reuse the same permit
-    # list between create and update. Also, you can specialize this method
-    # with per-user checking of permissible attributes.
-    # Strong Parameters
-    def recipe_params
-      params.require(:recipe).permit(:name, :description)
+  def edit
+    @recipe = current_user.recipes.find(params[:id])
+  end
+
+  def update
+    @recipe = current_user.recipes.find(params[:id])
+
+    if @recipe.update_attributes(recipe_params)
+      redirect_to recipe_path(@recipe), notice: "Recipe Updated!"
+    else
+      @errors = @recipe.errors.full_messages
+      render :edit
     end
+  end
+
+  private
+
+  # Using a private method to encapsulate the permissible parameters is
+  # a good pattern since you'll be able to reuse the same permit
+  # list between create and update. Also, you can specialize this method
+  # with per-user checking of permissible attributes.
+  # Strong Parameters
+  def recipe_params
+    params.require(:recipe).permit(:name, :description)
+  end
 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   # this includes validations such as email uniqueness
   include Clearance::User
 
+  # provide User class with knowledge that it has recipes
+  has_many :recipes
+
 end
